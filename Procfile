adc25d54cff4c5c5b56fcc7b8e075188ac227ef3
zeganstyl <zeganstyl@gmail.com>
2020-12-13 22:41:05 +0500

    1

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: java -jar build/libs/kotme-kotlin-server.jar
+web: java -Dserver.port=$PORT $JAVA_OPTS -jar build/libs/kotme-kotlin-server.jar
--- src/main/kotlin/org/thelemistix/kotme/Main.kt
+++ src/main/kotlin/org/thelemistix/kotme/Main.kt
@@ -1,12 +1,12 @@
 package org.thelemistix.kotme
 
-import io.ktor.application.*
-import io.ktor.request.*
-import io.ktor.response.*
-import io.ktor.routing.*
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
-import org.json.simple.JSONObject
+//import io.ktor.application.*
+//import io.ktor.request.*
+//import io.ktor.response.*
+//import io.ktor.routing.*
+//import io.ktor.server.engine.*
+//import io.ktor.server.netty.*
+//import org.json.simple.JSONObject
 import java.io.ByteArrayOutputStream
 import java.io.PrintStream
 import javax.script.ScriptEngineManager
@@ -31,70 +31,70 @@ object Main {
     fun server(args: Array<String>) {
         //engine.eval("fun main(){}", bindings)
 
-        embeddedServer(
-            Netty,
-            port = args.getOrNull(0)?.toIntOrNull() ?: System.getenv()["PORT"]?.toIntOrNull() ?: 8888,
-            host = "localhost"
-        ) {
-            routing {
-                get("/") {
-                    call.respondText { "KOTme is running" }
-                }
-                post("/") {
-                    val param = call.receiveParameters()
-                    val code = param["code"]
-
-                    var message = ""
-                    var status = ResultStatus.TestsSuccess
-                    var consoleLog = ""
-
-                    if (code != null) {
-                        bindings.clear()
-                        os.reset()
-                        System.setOut(ps)
-
-                        try {
-                            engine.eval(code, bindings)
-
-                            message = when (param["exercise"]?.toIntOrNull()) {
-                                1 -> exe1()
-                                2 -> exe2()
-                                3 -> exe3()
-                                4 -> exe4()
-                                5 -> exe5()
-                                6 -> exe6()
-                                7 -> exe7()
-                                else -> {
-                                    status = ResultStatus.ServerError
-                                    "Не верно указан номер задачи"
-                                }
-                            }
-
-                            consoleLog = os.toString()
-
-                            if (message.isNotEmpty() && status == ResultStatus.TestsSuccess) {
-                                status = ResultStatus.TestsFail
-                            }
-                        } catch (ex: Exception) {
-                            status = ResultStatus.ExecutionErrors
-                            consoleLog = os.toString()
-                            message = "В коде есть ошибки.\n${ex.message}"
-                        }
-
-                        System.setOut(console)
-                    } else {
-                        status = ResultStatus.ServerError
-                        message = "Отсутствует исходный код по задаче"
-                    }
-
-                    val json = JSONObject()
-                    json["status"] = status
-                    json["message"] = message
-                    json["console"] = consoleLog
-                    call.respondText { json.toJSONString() }
-                }
-            }
-        }.start(wait = true)
+//        embeddedServer(
+//            Netty,
+//            port = args.getOrNull(0)?.toIntOrNull() ?: System.getenv("PORT")?.toIntOrNull() ?: 8888,
+//            host = "localhost"
+//        ) {
+//            routing {
+//                get("/") {
+//                    call.respondText { "KOTme is running" }
+//                }
+//                post("/") {
+//                    val param = call.receiveParameters()
+//                    val code = param["code"]
+//
+//                    var message = ""
+//                    var status = ResultStatus.TestsSuccess
+//                    var consoleLog = ""
+//
+//                    if (code != null) {
+//                        bindings.clear()
+//                        os.reset()
+//                        System.setOut(ps)
+//
+//                        try {
+//                            engine.eval(code, bindings)
+//
+//                            message = when (param["exercise"]?.toIntOrNull()) {
+//                                1 -> exe1()
+//                                2 -> exe2()
+//                                3 -> exe3()
+//                                4 -> exe4()
+//                                5 -> exe5()
+//                                6 -> exe6()
+//                                7 -> exe7()
+//                                else -> {
+//                                    status = ResultStatus.ServerError
+//                                    "Не верно указан номер задачи"
+//                                }
+//                            }
+//
+//                            consoleLog = os.toString()
+//
+//                            if (message.isNotEmpty() && status == ResultStatus.TestsSuccess) {
+//                                status = ResultStatus.TestsFail
+//                            }
+//                        } catch (ex: Exception) {
+//                            status = ResultStatus.ExecutionErrors
+//                            consoleLog = os.toString()
+//                            message = "В коде есть ошибки.\n${ex.message}"
+//                        }
+//
+//                        System.setOut(console)
+//                    } else {
+//                        status = ResultStatus.ServerError
+//                        message = "Отсутствует исходный код по задаче"
+//                    }
+//
+//                    val json = JSONObject()
+//                    json["status"] = status
+//                    json["message"] = message
+//                    json["console"] = consoleLog
+//                    call.respondText { json.toJSONString() }
+//                }
+//            }
+//        }.start()
     }
 
     fun script() {
