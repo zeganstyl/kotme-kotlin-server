4344f73576db3507ac78ecd026aa98daf94cc5a9
zeganstyl <zeganstyl@gmail.com>
2021-08-09 12:41:05 +0500

    1

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,6 +54,8 @@ tasks.jar {
         )
     }
 
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
     from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
 }
 
